fbb2170e2980e979477acfbc397d7e5d95c631ab
emmanueldesaintleger <emmanuelstleger@gmail.com>
2021-03-12 18:06:11 +0100

    gestion sécurité et déconnexion

--- src/Controller/PhotoController.php
+++ src/Controller/PhotoController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/photo")
+ * @Route("dashboard/photo")
  */
 class PhotoController extends AbstractController
 {
